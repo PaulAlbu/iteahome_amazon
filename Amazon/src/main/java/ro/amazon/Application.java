7fbf761edeef049e20d63d8c6f22196c3a7263f0
Alin Ciortea <ciortea.alin@gmail.com>
2023-12-10 15:52:43 +0200

    Add login functionality

--- Amazon/src/main/java/ro/amazon/Application.java
+++ Amazon/src/main/java/ro/amazon/Application.java
@@ -1,8 +1,11 @@
 package ro.amazon;
 
+import ro.amazon.ui.UserLogin;
+
 public class Application {
     public static void main(String[] args) {
-
+        UserLogin login = new UserLogin();
+        login.login();
         // Service
         //ProductDisplay
         //AddToCart
