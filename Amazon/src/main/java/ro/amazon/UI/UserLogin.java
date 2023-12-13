03c123c9afb302d566cd9a5bc5b5f081e3ffef79
Alin Ciortea <ciortea.alin@gmail.com>
2023-12-13 20:03:38 +0200

    Login functionality

--- Amazon/src/main/java/ro/amazon/UI/UserLogin.java
+++ Amazon/src/main/java/ro/amazon/UI/UserLogin.java
@@ -1,7 +1,40 @@
 package ro.amazon.UI;
 
 import ro.amazon.controller.UserController;
+import ro.amazon.exceptions.InvalidCredentialsException;
+
+import java.security.InvalidParameterException;
+import java.util.Scanner;
 
 public class UserLogin {
     UserController userController = new UserController();
+
+    public boolean login() {
+        boolean isLoginSuccessful = false;
+        int loginAttempts = 0;
+
+        while (!isLoginSuccessful && loginAttempts < 3) {
+            try {
+                System.out.println("Username: ");
+                Scanner keyboard = new Scanner(System.in);
+                String username = keyboard.next();
+
+                System.out.println("Password: ");
+                String password = keyboard.next();
+
+                userController.login(username, password);
+                isLoginSuccessful = true;
+                System.out.println("Login successful");
+            } catch (InvalidCredentialsException e) {
+                System.out.println("Invalid credentials");
+                isLoginSuccessful = false;
+                loginAttempts++;
+            } catch (InvalidParameterException e) {
+                System.out.println("Invalid credentials format");
+                isLoginSuccessful = false;
+                loginAttempts++;
+            }
+        }
+        return isLoginSuccessful;
+    }
 }
