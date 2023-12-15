b51f71196ba3f2b90c8aafb7385d640c4f5c8432
Name-Alin <79106266+Name-Alin@users.noreply.github.com>
2023-12-15 18:30:45 +0200

    Add login functionality (#1)

--- Amazon/src/main/java/ro/amazon/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package ro.amazon.controller;
-
-import ro.amazon.service.UserService;
-
-public class UserController {
-    UserService userService = new UserService();
-}
--- Amazon/src/main/java/ro/amazon/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package ro.amazon.service;
-
-import ro.amazon.dao.UserDAO;
-
-public class UserService {
-    UserDAO userDAO = new UserDAO();
-}
