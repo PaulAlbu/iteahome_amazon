801131701546fc00f321fed99afe0bc83b68b01c
Paul Albu <paul.i.albu@gmail.com>
2023-12-17 19:01:55 +0200

    Created product feature

--- src/main/java/ro/amazon/controller/ProductController.java
+++ src/main/java/ro/amazon/controller/ProductController.java
@@ -1,7 +1,27 @@
 package ro.amazon.controller;
 
+import ro.amazon.entity.Product;
+import ro.amazon.exceptions.PriceErrorException;
+import ro.amazon.exceptions.ProductDatabaseException;
 import ro.amazon.service.ProductService;
 
+import java.util.ArrayList;
+
 public class ProductController {
-    ProductService productService = new ProductService();
+    private ProductService productService = new ProductService();
+
+    public void displayProductsList() {
+        try {
+            ArrayList<Product> productsList = productService.validateProductsList();
+            for (Product product: productsList ) {
+                System.out.println(product.getName() + "; Specifications:  " + product.getProductDescription() + "; Price: " + product.getPrice() + " EUR");
+            }
+        } catch (PriceErrorException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e);
+        } catch (ProductDatabaseException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e);
+        }
+    }
 }
---
+++ src/main/java/ro/amazon/exceptions/ProductDatabaseException.java
@@ -0,0 +1,7 @@
+package ro.amazon.exceptions;
+
+public class ProductDatabaseException extends Exception {
+    public ProductDatabaseException(){
+        super("Product Data Base issue");
+    }
+}
